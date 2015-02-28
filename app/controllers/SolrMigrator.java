20101581dda346e3dcb37bbb837ee06cecb1963d
Jason R Alexander <sunnysidesounds@gmail.com>
2015-02-27 23:07:16 -0800

    Working insertion process

--- app/controllers/SolrMigrator.java
+++ app/controllers/SolrMigrator.java
@@ -7,6 +7,8 @@ import play.mvc.Result;
 import helpers.*;
 import play.Logger;
 
+import java.math.BigDecimal;
+
 public class SolrMigrator extends Controller {
 
     public static Result index(){
@@ -17,74 +19,30 @@ public class SolrMigrator extends Controller {
 
     public static Result importData(){
 
-        int maxRows = 500;
-        int batchSize = 100;
+        int maxRows = Solr.solrMaxRows;
+        int batchSize = Solr.solrBatchSize;
         int numberOfBatches = maxRows / batchSize;
         int startPoint = 0;
         int endPoint = 0;
-
         System.out.println();
         System.out.println("number of batches: " + numberOfBatches);
 
         for(int i = 1, j = 0; i <= numberOfBatches; i++, j++){
             startPoint = (j * batchSize);
             endPoint = (startPoint + batchSize);
-
-
-            System.out.println("start: " + startPoint + " rows: " + batchSize);
+            Solr.buildInsert(startPoint, batchSize);
         }
 
         System.out.println("end point: " + endPoint);
 
         if(maxRows != endPoint){
             batchSize = (maxRows - endPoint);
-            System.out.println("New batch size: " + batchSize);
-
+            Solr.buildInsert(endPoint, batchSize);
         }
 
 
 
 
-
-
-
-
-       /*
-
-        JsonNode rawResponse = Requests.get(Solr.buildQuery());
-        JsonNode contentResponse = rawResponse.get("response").get("docs");
-        int successful = 0;
-        int failure = 0;
-        for(JsonNode content : contentResponse){
-            System.out.println(content);
-
-            String title = content.get("title").asText() == null ? content.get("title").asText() : "no title";
-
-
-            String id = content.get("id").asText() == null ? content.get("id").asText() : "no id";
-            String title = content.get("title").asText() == null ? content.get("title").asText() : "no title";
-            String ctn = content.get("content").asText() == null ? content.get("content").asText() : "no content";
-            String url = content.get("url").asText() == null ? content.get("url").asText() : "no url";
-            String segment = content.get("segment").asText() == null ? content.get("segment").asText() : "no segment";
-            String digest = content.get("digest").asText() == null ? content.get("digest").asText() : "no digest";
-            String boost = content.get("boost").asText() == null ? content.get("boost").asText() : "no boost";
-            String cache = content.get("cache").asText() == null ? content.get("cache").asText() : "no cache";
-            String tstamp = content.get("tstamp").asText() == null ? content.get("tstamp").asText() : "no tstamp";
-            if(SolrData.getById(id) == null){
-                SolrData data = SolrData.setData(id, title, url, ctn, segment, digest, boost, cache, tstamp);
-                Logger.info("Solr ID save to DB: " + data.id);
-                successful++;
-
-            } else {
-                Logger.info("Solr ID already in DB: " + id);
-                failure++;
-            }
-
-
-        }
-        //return ok(successful + " successfully imported, " + failure + " failed to import!");
-        return ok(rawResponse);
-          */
 
         return ok("");
 
--- app/helpers/Solr.java
+++ app/helpers/Solr.java
@@ -1,5 +1,7 @@
 package helpers;
 
+import models.SolrData;
+import org.codehaus.jackson.JsonNode;
 import play.Logger;
 import play.Play;
 
@@ -8,28 +10,60 @@ public class Solr {
 
     public static final String solrBaseUrl = Play.application().configuration().getString("solr.base.url");
     public static final String solrQuery = Play.application().configuration().getString("solr.query");
-    public static final Integer solrStart = Play.application().configuration().getInt("solr.start");
-    public static final Integer solrRows = Play.application().configuration().getInt("solr.rows");
+    public static final Integer solrMaxRows = Play.application().configuration().getInt("solr.max.rows");
+    public static final Integer solrBatchSize = Play.application().configuration().getInt("solr.batch.size");
     public static final String solrFormat = Play.application().configuration().getString("solr.format");
 
     /**
      * This build the solr url that will use to query solr service.
      * @return String url
      */
-    public static String buildQuery(){
+    public static String buildQuery(int start, int rows){
         if(solrBaseUrl == null
                 || solrQuery == null
-                || solrStart == null
-                || solrRows == null
                 || solrFormat == null){
             Logger.error("One of your Solr configuration values is null. Check your application.conf");
         }
-
-        return solrBaseUrl + "solr/select/?q="
+        String url = solrBaseUrl + "solr/select/?q="
                 + solrQuery + "&start="
-                + solrStart + "&rows="
-                + solrRows + "&wt="
+                + start + "&rows="
+                + rows + "&wt="
                 + solrFormat;
+        Logger.info("Starting at [" + start + "] with [" + rows + "] rows -- Using url [" + url + "]");
+        System.out.println();
+        return url;
+    }
+
+    /**
+     * This get the response from solr instance and builds values and inserts into db.
+     * @param start the start point in rows pagination
+     * @param rows of many rows at a time to insert
+     */
+    public static void buildInsert(int start, int rows){
+        JsonNode rawResponse = Requests.get(Solr.buildQuery(start, rows)).get("response").get("docs");
+        for(JsonNode response : rawResponse) {
+            String id = response.get("id").getTextValue();
+            String title = response.get("title").getTextValue();
+            String content = response.get("content").getTextValue();
+            String url = response.get("url").getTextValue();
+            String segment = response.get("segment").getTextValue();
+            String boost = response.get("boost").getDecimalValue().toString();
+            String cache = response.get("cache").getTextValue();
+            String digest = response.get("digest").getTextValue();
+            String tstamp = response.get("tstamp").getTextValue();
+
+            if (SolrData.getById(id) == null) {
+                SolrData data = SolrData.setData(id, title, url, content, segment, digest, boost, cache, tstamp);
+                Logger.info("Solr ID save to DB: " + data.id);
+            } else {
+                Logger.error("Solr ID already in DB: " + id);
+            }
+        }
+
+
+
+
+
     }
 
 }
