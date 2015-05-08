a4f133bb41697752bd522de0a7ded1c18abacfeb
Jason R Alexander <sunnysidesounds@gmail.com>
2015-05-07 22:21:20 -0700

    Updates for db insertion to recipe / ingredients

--- app/controllers/DOMParser.java
+++ app/controllers/DOMParser.java
@@ -1,6 +1,6 @@
 package controllers;
 
-import models.EpicuriousData;
+import models.*;
 import org.jsoup.Jsoup;
 import org.jsoup.helper.Validate;
 import org.jsoup.nodes.Document;
@@ -18,44 +18,69 @@ public class DOMParser extends Controller {
     public static Result parseEpicurious() throws IOException{
 
         int count = 0;
-        int limit = 2;
+        int limit = 33253;
         String baseUrl = "http://www.epicurious.com";
         List<EpicuriousData> data = EpicuriousData.getAll();
 
         for(EpicuriousData value : data){
-            Document doc = Jsoup.connect(value.id).get();
 
-            System.out.println("-----------------------------------------");
-            System.out.println("RECIPE ID: " + value.autoId + " -- URL:" + value.id);
-            System.out.println("");
+            if(Recipe.getBySourceUrl(value.id) == null){
+                Document doc = Jsoup.connect(value.id).get();
 
-            //Get title
-            System.out.println("TITLE: ");
-            Elements title = doc.select("#headline h1");
-            System.out.println(title.text());
-            System.out.println("");
 
-            // Get photo
-            System.out.println("PHOTO: ");
-            String relativeUrl = doc.select(".photo").attr("src");
-            if(!relativeUrl.equals("")) {
-                String photo = baseUrl + relativeUrl;
-                System.out.println(photo);
+                System.out.println("-----------------------------------------");
+                System.out.println("RECIPE ID: " + value.autoId + " -- URL:" + value.id);
+                System.out.println("");
+
+                //Get title
+                System.out.println("TITLE: ");
+                Elements title = doc.select("#headline h1");
+                System.out.println(title.text());
+                System.out.println("");
+
+                // Get photo
+                String photo = null;
+                System.out.println("PHOTO: ");
+                String relativeUrl = doc.select(".photo").attr("src");
+                if(!relativeUrl.equals("")) {
+                    photo = baseUrl + relativeUrl;
+                    System.out.println(photo);
+                }
+
+
+                System.out.println("");
+
+                // Get preparation
+                Elements preparation = doc.select("#preparation");
+                System.out.println("PREPARATION: ");
+                System.out.println(preparation.text());
+
+
+                Recipe saved = Recipe.setData(
+                        title.text(),
+                        value.id,
+                        photo,
+                        preparation.text()
+                );
+
+                // Get ingredients
+                System.out.println("INGREDENTS: ");
+                Elements ingredientParent = doc.select(".ingredient");
+                for(Element ingredientChild : ingredientParent ){
+                    System.out.println(ingredientChild.text());
+                    Ingredient.setData(
+                            saved.id,
+                            ingredientChild.text(),
+                            null
+                    );
+
+                }
+                System.out.println("");
+
+            } else {
+                System.out.println(value.id + " already exists");
             }
-            System.out.println("");
 
-            // Get ingredients
-            System.out.println("INGREDENTS: ");
-            Elements ingredientParent = doc.select(".ingredient");
-            for(Element ingredientChild : ingredientParent ){
-                System.out.println(ingredientChild.text());
-
-            }
-            System.out.println("");
-            // Get preparation
-            Elements preparation = doc.select("#preparation");
-            System.out.println("PREPARATION: ");
-            System.out.println(preparation.text());
 
 
 
